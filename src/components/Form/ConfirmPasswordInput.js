08319e71e83b19cde22c3919d37c4442687e49bd
Kleber Molina Kardel <molinakardel@gmail.com>
2023-07-01 11:36:15 -0300

    Nova funcionalidade: Cria funcionalidade de exibir e esconder senhas nos dois inputs ao clicar no ícone de visualização

--- src/components/Form/ConfirmPasswordInput.js
+++ src/components/Form/ConfirmPasswordInput.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const ConfirmPasswordInput = ({ value, onChange, disabled }) => {
-    return (
-        <div>
-            <label htmlFor="confirmPassword">Confirme sua senha: </label>
-            <input 
-                type="password"
-                id="confirmPassword"
-                value={value}
-                onChange={onChange}
-                disabled={disabled}
-            />
-        </div>
-    )
-}
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label htmlFor="confirmPassword">Confirme sua senha:</label>
+      <div className="password-input">
+        <input
+          type={showPassword ? 'text' : 'password'}
+          id="confirmPassword"
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+        />
+        <button
+          type="button"
+          onClick={handleTogglePassword}
+          disabled={disabled}
+        >
+          {showPassword ? (
+            <FontAwesomeIcon icon={faEyeSlash} />
+          ) : (
+            <FontAwesomeIcon icon={faEye} />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ConfirmPasswordInput;
--- src/components/Form/PasswordInput.js
+++ src/components/Form/PasswordInput.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const PasswordInput = ({ value, onChange }) => {
-    return (
-        <div>
-            <label htmlFor="password">Digite sua senha: </label>
-            <input 
-                type="password"
-                id="password"
-                value={value}
-                onChange={onChange}
-            />
-        </div>
-    )
-}
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label htmlFor="password">Digite sua senha:</label>
+      <div className="password-input">
+        <input
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          value={value}
+          onChange={onChange}
+        />
+        <button type="button" onClick={handleTogglePassword}>
+          {showPassword ? (
+            <FontAwesomeIcon icon={faEyeSlash} />
+          ) : (
+            <FontAwesomeIcon icon={faEye} />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default PasswordInput;
