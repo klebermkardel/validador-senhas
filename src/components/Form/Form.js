90207c492c21337707aae4001e5576375e4c8628
Kleber Molina Kardel <molinakardel@gmail.com>
2023-07-01 10:59:02 -0300

    Nova funcionalidade: inicializa o estado para password e confirmPassword usando o useState do React

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PasswordInput from './PasswordInput';
 import ConfirmPasswordInput from './ConfirmPasswordInput';
 import SubmitButton from './SubmitonButton';
 
 
 const Form = () => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <form>
         <PasswordInput />
